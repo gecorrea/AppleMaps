87ecb4a27266f137fd4171be7a5013d121f2d2a7
George <george.e.correa@gmail.com>
2017-04-25 18:05:46 -0400

    Used storyboard to set up mapView. On start up, Map will zoom in on current location of the user.

--- AppleMaps/ViewController.swift
+++ AppleMaps/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    var locationManager:CLLocationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var logo: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mapView.delegate = self
+        mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +33,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selectMapView(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = MKMapType.standard
+            break
+        case 1:
+            mapView.mapType = MKMapType.hybrid
+            break
+        case 2:
+            mapView.mapType = MKMapType.satellite
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView.setRegion(MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 200, 200), animated: true)
+    }
 
 }
 
