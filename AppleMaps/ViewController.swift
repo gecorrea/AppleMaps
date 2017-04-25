03e42fb0eeb113996707b5d1f066ccb7c0758fc9
George <george.e.correa@gmail.com>
2017-04-25 12:03:17 -0400

    Adding Xcode project.

---
+++ AppleMaps/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppleMaps
+//
+//  Created by Aditya Narayan on 4/25/17.
+//  Copyright © 2017 George Correa. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
